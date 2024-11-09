b172e38da214a16c84ef37e128633050c7c189be
DinaMazur <146865201+DinaMazur@users.noreply.github.com>
2024-11-09 14:14:43 +0200

    Add files via upload

---
+++ hw3.js
@@ -0,0 +1,29 @@
+// 1
+
+const result = 5 + 5 + '5';
+console.log(result);
+
+// 2
+
+const email = "mazurdr00@gmail.com";
+console.log(email.includes('@'));
+
+
+
+// 3
+
+const first = "My";
+const second = "name";
+const third = "is"
+const fullName = first + " " + second + " " + third + " " + 'Viktor' ;
+console.log(fullName);
+
+
+// 4
+
+const userName = "Олено";
+const  payment = "300 гривень"
+const end = 'Дякуэмо' + ' ' + userName + ' ' + 'до сплати' + ' ' + payment;
+alert(end);
+
+
